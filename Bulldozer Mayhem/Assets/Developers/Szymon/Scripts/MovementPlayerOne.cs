648ac9c15f428ecf3d526c036692f39045c6ee88
Szymonstasiewicz <szymonstasiewicz06@outlook.com>
2024-11-14 14:43:02 +0100

    Verdere movement nu kan die links en rechts draaien maar is nog steeds beetje dookie

---
+++ Bulldozer Mayhem/Assets/Developers/Szymon/Scripts/MovementPlayerOne.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayerOne : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    
+    void Update()
+    {
+        //Move forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.forward * Time.deltaTime);
+            Debug.Log("W key was pressed");
+        }
+
+        //Move right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+
+        //Move left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+        }
+
+        //Move back
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-transform.forward * Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ Bulldozer Mayhem/Assets/Developers/Szymon/Scripts/MovementPlayerTwo.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayerTwo : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //Move forward
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(transform.forward * Time.deltaTime);
+            Debug.Log("W key was pressed");
+        }
+
+        //Move right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+
+        //Move left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+        }
+
+        //Move back
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-transform.forward * Time.deltaTime);
+        }
+
+
+    }
+}
