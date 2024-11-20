ce8d65c9aa7cdaf038eb6b193f9b060c15c43ccc
Szymonstasiewicz <szymonstasiewicz06@outlook.com>
2024-11-20 14:29:48 +0100

    bygygy

---
+++ Bulldozer Mayhem/Assets/Developers/Szymon/(AFBLIJVEN) Scripts voor boundary/Scriptmovementtest.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Scriptmovementtest : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField] private float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        //Move forward
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.Translate(transform.forward * Time.deltaTime);
+
+
+        }
+
+        //Move right
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+
+
+        }
+
+        //Move left
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+
+        }
+
+        //Move back
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.Translate(-transform.forward * Time.deltaTime);
+
+        }
+
+    }
+}
