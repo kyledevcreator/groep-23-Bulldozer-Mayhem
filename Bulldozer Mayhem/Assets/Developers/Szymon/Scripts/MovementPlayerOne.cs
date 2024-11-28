39a268d9def1949c3760aabcd433f0e4e7f9e852
kyledevcreator <houthuijzenk@gmail.com>
2024-11-28 12:15:24 +0100

    niks

--- Bulldozer Mayhem/Assets/Developers/Szymon/Scripts/MovementPlayerOne.cs
+++ Bulldozer Mayhem/Assets/Developers/Szymon/Scripts/MovementPlayerOne.cs
@@ -4,6 +4,7 @@ public class MovementPlayerOne : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 10f;
     [SerializeField] private float rotationSpeed;
+    private bool hasGrip;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,38 +13,54 @@ public class MovementPlayerOne : MonoBehaviour
 
     void Update()
     {
-        //Move forward
-        if (Input.GetKey(KeyCode.W))
+        if (hasGrip)
         {
-            //   transform.Translate(transform.forward * Time.deltaTime * movementSpeed, Space.World);
-            transform.Translate(0, 0, 1 * Time.deltaTime * movementSpeed);
+            //Move forward
+            if (Input.GetKey(KeyCode.W))
+            {
+                //   transform.Translate(transform.forward * Time.deltaTime * movementSpeed, Space.World);
+                transform.Translate(0, 0, 1 * Time.deltaTime * movementSpeed);
 
+            }
+
+            //Move right
+            if (Input.GetKey(KeyCode.D))
+            {
+
+                transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+
+
+            }
+
+            //Move left
+            if (Input.GetKey(KeyCode.A))
+            {
+
+                transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+
+            }
+
+            //Move back
+            if (Input.GetKey(KeyCode.S))
+            {
+
+                transform.Translate(0, 0, -1 * Time.deltaTime * movementSpeed);
+
+            }
         }
 
-        //Move right
-        if (Input.GetKey(KeyCode.D))
+
+
+        RaycastHit hit;
+
+        // Perform the raycast
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 2))
         {
-
-            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
-
-
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 2, Color.yellow, 2);
+                hasGrip = true; // Set hasGrip to true when the ray hits
         }
 
-        //Move left
-        if (Input.GetKey(KeyCode.A))
-        {
-
-            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
-
-        }
-
-        //Move back
-        if (Input.GetKey(KeyCode.S))
-        {
-
-            transform.Translate(0, 0, -1 * Time.deltaTime * movementSpeed);
-
-        }
+        // Note: Do not reset `hasGrip` to `false` outside this condition.
 
     }
 }
