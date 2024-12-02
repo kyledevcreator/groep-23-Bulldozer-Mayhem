c279c23f8ee8f58564c4a203ee58b0ff4e2e5b96
Jordy van der Meer <jordyvandermeer10@gmail.com>
2024-12-02 10:26:18 +0100

    Pushback

--- Bulldozer Mayhem/Assets/Developers/Jordy/Scripts/PlayerRespawn.cs
+++ Bulldozer Mayhem/Assets/Developers/Jordy/Scripts/PlayerRespawn.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
+    public Rigidbody rb;
+    public Rigidbody rb2;
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
             {
                 other.gameObject.transform.position = Vector3.up;
+                other.gameObject.transform.rotation = Quaternion.identity;
+                rb.velocity = Vector3.zero;
+                rb2.velocity = Vector3.zero;
             }
     }
 }
