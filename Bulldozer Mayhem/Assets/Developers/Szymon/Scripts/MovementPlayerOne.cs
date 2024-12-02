324488a1fde1aebf02e80126e5bf88f5b044e54a
Szymonstasiewicz <szymonstasiewicz06@outlook.com>
2024-12-02 12:24:38 +0100

    De movement is nu het beste dat het kan zijn
    De bulldozers zweven niet meer in de lucht

--- Bulldozer Mayhem/Assets/Developers/Szymon/Scripts/MovementPlayerOne.cs
+++ Bulldozer Mayhem/Assets/Developers/Szymon/Scripts/MovementPlayerOne.cs
@@ -4,21 +4,41 @@ public class MovementPlayerOne : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 10f;
     [SerializeField] private float rotationSpeed;
-    //private bool hasGrip;
+    private bool hasGrip;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private enum PlayerEnum
     {
+        Player1,
+        Player2
     }
 
+    [SerializeField] private PlayerEnum player;
+
+    // Start is called before the first frame update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+         if (collision.gameObject.layer == 3)
+         {
+             hasGrip = true;
+         }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            hasGrip = false;
+        }
+    }
 
     void Update()
     {
-            //Move forward
+        if (hasGrip && player == PlayerEnum.Player1)
+        {
             if (Input.GetKey(KeyCode.W))
             {
-                //   transform.Translate(transform.forward * Time.deltaTime * movementSpeed, Space.World);
+
                 transform.Translate(0, 0, 1 * Time.deltaTime * movementSpeed);
 
             }
@@ -47,21 +67,63 @@ public class MovementPlayerOne : MonoBehaviour
                 transform.Translate(0, 0, -1 * Time.deltaTime * movementSpeed);
 
             }
+        }
+        else if (hasGrip && player == PlayerEnum.Player2)
+        {
+            //Move forward
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                //   transform.Translate(transform.forward * Time.deltaTime * movementSpeed, Space.World);
+                transform.Translate(0, 0, 1 * Time.deltaTime * movementSpeed);
+
+            }
+
+            //Move right
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+
+                transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+
+
+            }
+
+            //Move left
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+
+                transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+
+            }
+
+            //Move back
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+
+                transform.Translate(0, 0, -1 * Time.deltaTime * movementSpeed);
+
+            }
+
+        }
+        //Move forward
 
 
 
 
-        //RaycastHit hit;
+        /*
+        RaycastHit hit;
 
-        //// Perform the raycast
-        //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 2))
-        //{
-        //        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 2, Color.yellow, 2);
-        //        hasGrip = true; // Set hasGrip to true when the ray hits
-        //}
+        // Perform the raycast
+        if (!Physics.Raycast(transform.position, Vector3.down, out hit, 1f))
+        {
+            hasGrip = false; // Set hasGrip to false when the ray doesn't hit
+        }
+        else
+        {
+            hasGrip = true; // Set hasGrip to true when the ray hits
+        }
 
         // Note: Do not reset `hasGrip` to `false` outside this condition.
-
+        */
     }
 }
 
