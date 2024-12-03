e9579de3a8da814fada6ec1c6644635db9188084
kyledevcreator <houthuijzenk@gmail.com>
2024-12-03 13:48:32 +0100

    Update sparks.cs

--- Bulldozer Mayhem/Assets/developers/Kyle/scripts/sparks.cs
+++ Bulldozer Mayhem/Assets/developers/Kyle/scripts/sparks.cs
@@ -10,23 +10,12 @@ public class sparks : MonoBehaviour
 
     // Start is called before the first frame update
 
-    void Start()
-    {
-        sparksVFX.Stop();
-
-    }
-    IEnumerator ExplosionTime()
-    {
-        yield return new WaitForSeconds(1f);
-        //Destroy(gameObject);
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            StartCoroutine(ExplosionTime());
             Explosion();
+            sparksVFX.Stop();
         }
 
     }
