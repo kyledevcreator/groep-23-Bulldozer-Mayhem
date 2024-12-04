f0ba06b73e0130af6b9319a797a8ae2a189188cc
kyledevcreator <houthuijzenk@gmail.com>
2024-12-04 15:26:56 +0100

    drivinf sounds

--- Bulldozer Mayhem/Assets/developers/Kyle/scripts/DrivingSounds.cs
+++ Bulldozer Mayhem/Assets/developers/Kyle/scripts/DrivingSounds.cs
@@ -6,17 +6,28 @@ public class DrivingSounds : MonoBehaviour
 {
 
     public AudioSource TrackSound;
+    private float volume;
+    private float maxVolume = 0.071f;
+    private float transitionSpeed = 2;
+    private MovementPlayerOne movementPlayer;
+    private void Awake()
+    {
+        movementPlayer = GetComponent<MovementPlayerOne>();
+
+    }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.S) || (Input.GetKey(KeyCode.D) &&
-            (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.RightArrow)))))))))
-        {
-            TrackSound.enabled = true;
-        }
-        else
-        {
-            TrackSound.enabled = false;
-        }
+
+        bool isMovingP1 = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+        bool isMovingP2 = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow);
+
+
+        float targetVolume = isMovingP1 ? maxVolume : 0;
+        if (movementPlayer.player == MovementPlayerOne.PlayerEnum.Player2)
+            targetVolume = isMovingP2 ? maxVolume : 0;
+
+        volume = Mathf.Lerp(volume, targetVolume, Time.deltaTime * transitionSpeed);
+        TrackSound.volume = volume;
     }
 }
