450dfb5d165ebd6856828ab9b6d647ee6ff7d217
kyledevcreator <houthuijzenk@gmail.com>
2024-12-04 13:45:29 +0100

    escape button aangemaakt

--- Bulldozer Mayhem/Assets/developers/Kyle/scripts/Buttons.cs
+++ Bulldozer Mayhem/Assets/developers/Kyle/scripts/Buttons.cs
@@ -5,21 +5,66 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public KeyCode resetTimeKey = KeyCode.Space; 
+    public float transitionDuration = 2.0f; 
+
+    private bool isResetting = false; 
+    private float elapsedTime = 0.0f; 
+
+
     private void Start()
     {
         Time.timeScale = 1;
     }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(resetTimeKey) && !isResetting)
+        {
+            isResetting = true;
+            elapsedTime = 0.0f;
+        }
+
+        if (isResetting)
+        {
+            elapsedTime += Time.unscaledDeltaTime; 
+            float t = elapsedTime / transitionDuration;
+
+            Time.timeScale = Mathf.Lerp(0, 1, t);
+
+
+            if (t >= 1.0f)
+            {
+                Time.timeScale = 1.0f;
+                isResetting = false;
+            }
+        }
+
+        // Back to menu with Escape key
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            BackToMenu(); // Call BackToMenu when Escape is pressed
+        }
+    }
+
+    void OnDisable()
+    {
+        Time.timeScale = 1.0f;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("MainGame");
         Time.timeScale = 1;
     }
 
-    //public void Options()
-    //{
-    //    SceneManager.LoadScene(3);
-    //}
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
 
     public void Quitgame()
     {
